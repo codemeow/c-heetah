4168ea19c9986017108e57fd9c7282e68bd84149
Шишкин Алексей <ALEXCONES@GMAIL.COM>
2016-05-18 15:17:37 +0300

    Added -Wall

--- examples/buildexamples.sh
+++ examples/buildexamples.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-gcc 00*.c              -o ./bin/00.elf
-gcc 01*.c ../cheetah.c -o ./bin/01.elf
-gcc 02*.c ../cheetah.c -o ./bin/02.elf
-gcc 03*.c ../cheetah.c -o ./bin/03.elf
-gcc 04*.c ../cheetah.c -o ./bin/04.elf -DCTH_ACTION_EXIT
-gcc 05*.c ../cheetah.c -o ./bin/05.elf -DCTH_OUTPUT_FILE
-gcc 06*.c ../cheetah.c -o ./bin/06.elf -DCTH_OUTPUT_STDERR
-gcc 07*.c ../cheetah.c -o ./bin/07.elf -DCTH_ACTION_EXIT -DCTH_COLOURING_ON
+gcc -Wall 00*.c              -o ./bin/00.elf
+gcc -Wall 01*.c ../cheetah.c -o ./bin/01.elf
+gcc -Wall 02*.c ../cheetah.c -o ./bin/02.elf
+gcc -Wall 03*.c ../cheetah.c -o ./bin/03.elf
+gcc -Wall 04*.c ../cheetah.c -o ./bin/04.elf -DCTH_ACTION_EXIT
+gcc -Wall 05*.c ../cheetah.c -o ./bin/05.elf -DCTH_OUTPUT_FILE
+gcc -Wall 06*.c ../cheetah.c -o ./bin/06.elf -DCTH_OUTPUT_STDERR
+gcc -Wall 07*.c ../cheetah.c -o ./bin/07.elf -DCTH_ACTION_EXIT -DCTH_COLOURING_ON
